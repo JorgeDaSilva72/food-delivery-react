2e877b3c4507c5000ca80e17734ee65809d94d8b
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-05-18 07:55:29 +0100

    Creation du bottom Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./App.css";
+import { useEffect } from "react";
 import Header from "./Components/Header";
+import MenuContainer from "./Components/MenuContainer";
+import {
+  AccountBalanceWalletRounded,
+  Chat,
+  Favorite,
+  HomeRounded,
+  Settings,
+  SummarizeRounded,
+} from "@mui/icons-material";
 
 function App() {
+  useEffect(() => {
+    const menuLi = document.querySelectorAll("#menu li");
+
+    function setMenuActive() {
+      menuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    menuLi.forEach((n) => n.addEventListener("click", setMenuActive));
+  }, []);
+
   return (
     <div className="App">
-      <Header />
       {/* header section */}
+      <Header />
       {/* main container */}
+      <main>
+        <div className="mainContainer"></div>
+        <div className="rightMenu"></div>
+      </main>
       {/* bottom menu */}
+      <div className="bottomMenu">
+        <ul id="menu">
+          <MenuContainer link={"#"} icon={<HomeRounded />} />
+          <MenuContainer link={"#"} icon={<Chat />} />
+          <MenuContainer link={"#"} icon={<AccountBalanceWalletRounded />} />
+          <MenuContainer link={"#"} icon={<Favorite />} />
+          <MenuContainer link={"#"} icon={<SummarizeRounded />} />
+          <MenuContainer link={"#"} icon={<Settings />} />
+          <div className="indicator"></div>
+        </ul>
+      </div>
     </div>
   );
 }
